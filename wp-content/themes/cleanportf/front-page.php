efa057522ac7ae52bed97bf6b2e544930c2a48d0
rafandrade <rafandrade.a@gmail.com>
2019-09-15 19:56:15 -0300

    add my github profile

--- wp-content/themes/cleanportf/front-page.php
+++ wp-content/themes/cleanportf/front-page.php
@@ -5,23 +5,21 @@
 
 		<div id="lineFooter">
 			<div class="container">
-				<p>Designer pela UEPA com trajetória multidisciplinar intercalando de artes visuais a webdesign. Aqui você encontrará alguns de meus trabalhos, bem como meu <a href="http://rafandrade.com.br/contato/">contato</a> e <a href="http://rafandrade.com.br/wp-content/uploads/2017/05/Curriculo_Rafael_Andrade.pdf">currículo</a>.</p>
+				<p>Designer pela UEPA com trajetória multidisciplinar intercalando de webdesign as artes visuais. Aqui você encontrará alguns de meus trabalhos, bem como meu <a href="http://rafandrade.com.br/contato/">contato</a>, meu <a href="https://github.com/RafaAndradee">gitHub</a> e <a href="http://rafandrade.com.br/wp-content/uploads/2017/05/Curriculo_Rafael_Andrade.pdf">currículo</a>.</p>
 			</div>
 		</div>
 		<div class="anime">
 			<?php get_template_part( 'template-parts/grid' ); ?>
-		</div>	
+		</div>
 	</div>
 
 	<div id="blog">
 		<?php get_template_part( 'template-parts/grid-blog' ); ?>
 	</div>
 
-	<div id="posts-blog">		
+	<div id="posts-blog">
 		<?php get_template_part( 'template-parts/posts-blog' ); ?>
 	</div>
 
 
 <?php get_footer(); ?>
-
-
